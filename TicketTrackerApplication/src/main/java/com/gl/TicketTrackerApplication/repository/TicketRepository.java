bc0d0ee69287ec4f8161f433f0a1c707482685ea
Kamal Malik <malik.kamal1631@gmail.com>
2024-05-18 22:10:25 +0530

    Add files via upload

---
+++ TicketTrackerApplication/src/main/java/com/gl/TicketTrackerApplication/repository/TicketRepository.java
@@ -0,0 +1,17 @@
+package com.gl.TicketTrackerApplication.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.gl.TicketTrackerApplication.entity.Ticket;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+	@Query("SELECT t from Ticket t WHERE " +
+            " t.title LIKE CONCAT('%', :query, '%') OR " +
+            " t.shortDescription LIKE CONCAT('%', :query, '%')")
+	List<Ticket> searchTickets(String query);
+
+}
